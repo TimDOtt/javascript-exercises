d500291e892d06c8840c25f9b04d8477ff93ea3d
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-08 18:16:47 -0500

    repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = '';
+    if (num < 0) return "ERROR";
+    while (num > 0){
+        string += str;
+        num--;
+    }
+    return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
