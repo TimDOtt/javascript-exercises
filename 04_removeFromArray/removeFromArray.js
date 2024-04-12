85fda22119b0c9adcb9648b5b39721798630fe41
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-12 13:07:28 -0500

    Complete 03, 04 and 05 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (i=0; i < arguments.length; i++){
+        for (j=0; j < array.length; j++){
+            if (arguments[i] === array[j]) {
+                array.splice(j, 1);
+                j--
+            }
+        }
+    }
+    return array; 
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let value;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        value = num2;
+        while(num2 != num1) {
+            value += num2 + 1;
+            num2++;
+        }
+    }else {
+        value = num1;
+        while(num1 != num2) {
+            value += num1 + 1;
+            num1++;
+        }
+    }
+    return value;
 };
 
 // Do not edit below this line
