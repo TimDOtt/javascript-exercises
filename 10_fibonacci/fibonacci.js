271d7ff9060a3066dae2b834d30924061992c30b
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-05-05 15:57:33 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  a = 1;
+  b = 0;
 
+  if (typeof number === "string") {
+    number = Number(number);
+  }
+  if (number === 0) {
+    a = number;
+  } else if (number < 0) {
+    a = "OOPS";
+  }
+  for (i = 1; i < number; i++) {
+    a = a + b;
+    b = a - b;
+  }
+  return a;
 };
 
 // Do not edit below this line
