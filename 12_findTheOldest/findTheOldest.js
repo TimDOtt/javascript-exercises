db89c7a3432235c340ec28061176403055b5dcfc
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-05-05 17:10:59 -0500

    Complete getTheTitles and findTheOldest exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce((last, current) => {
+    const oldAge = getAge(last.yearOfBirth, last.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+    return oldAge < currentAge ? current : last;
+  });
+
+  function getAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 };
 
 // Do not edit below this line
