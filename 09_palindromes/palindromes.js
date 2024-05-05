d0c81d3f27dc8c52d93a4226c83f7d7279b102f9
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-05-05 14:30:55 -0500

    Complete calculator and palindrome exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let lowerCase = str
+    .toLowerCase()
+    .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, "");
 
+  let reverseString = lowerCase.split("").reverse().join("");
+
+  if (reverseString === lowerCase) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
